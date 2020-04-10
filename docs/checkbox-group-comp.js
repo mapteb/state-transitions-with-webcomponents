1278a893f83e5e85a14f7689eb81096cc5275deb
Nalla Senthilnathan <nrsenthil@gmail.com>
2020-04-10 08:35:11 -0400

    updated web components with HTML Template feature

--- docs/checkbox-group-comp.js
+++ docs/checkbox-group-comp.js
@@ -10,10 +10,15 @@ class CheckboxGroupComp extends HTMLElement {
         if (newVal != null && newVal != undefined && newVal.length > 0 && name === 'data-request') {
             let data = JSON.parse(newVal);
             if (data != null && data.action === 'create') {
-                this.appendChild(this.createCheckboxElement(data));
+                if(this.innerHTML.length==0) {
+                    this.innerHTML = this.createCheckboxElement(data);
+                } else {
+                    this.innerHTML += this.createCheckboxElement(data);
+                }
             } else if (data != null && data.action === 'delete') {
-                console.log(">>> checkbox delete request: ", data.values);
-                data.values.forEach(i => this.removeChild(document.getElementById("div" + i)));
+                for(let i of data.values) {
+                    this.removeChild(document.getElementById("div" + i));
+                } 
             } else if (data != null && data.action === 'update') {
                 for(let el of this.getElementsByTagName('input')) {
                     if(el.value === data.value) {
@@ -41,24 +46,9 @@ class CheckboxGroupComp extends HTMLElement {
         const nextId = data.value;
         const label = data.todoText;
 
-        let de = document.createElement("div");
-        de.setAttribute("id", "div" + nextId);
-        let e = document.createElement("input");
-
-        e.setAttribute("name", name);
-        e.setAttribute("type", "checkbox");
-        e.setAttribute("value", nextId);
-        e = de.appendChild(e);
-
-        let lbl = document.createElement("label");
-        lbl.setAttribute("id", "lbl" + nextId);
-        lbl.innerText = label;
-        e.insertAdjacentElement("afterEnd", lbl)
-        return de;
-    }
-
-    deleteCheckboxElements(data) {
-        data.values.forEach(i => this.removeChild(document.getElementById("div" + i)));
+        return `<div id="div${nextId}">
+        <input type="checkbox" name="${name}" value="${nextId}"><label id="lbl${nextId}">${label}</label>
+        </div>`;
     }
 }
 window.customElements.define('checkbox-group-comp', CheckboxGroupComp);
--- docs/input-comp.js
+++ docs/input-comp.js
@@ -11,23 +11,15 @@ class InputComp extends HTMLElement {
         if (newVal != null && newVal != undefined && newVal.length > 0 && name === 'data-request') {
             const data = JSON.parse(newVal);
             if (data != null && data.action === 'create') {
-                this.innerHTML = this.createInputTxtElem(data).outerHTML;
+                this.innerHTML = this.createInputTxtElem(data);
             }
         }
     }
 
     createInputTxtElem(data) {
-        let de = document.createElement("div");
-        de.setAttribute("id", data.name + "_div");
-        let e = document.createElement("input");
-
-        e.setAttribute("name", data.name);
-        e.setAttribute("placeholder", "Enter your text...");
-        e.setAttribute("type", "text");
-        e.setAttribute("value", data.todoText);
-        e = de.appendChild(e);
-
-        return de;
+        return `<div id="${data.name}_div">
+                  <input type="text" name="${data.name}" placeholde="Enter text..." value="${data.todoText}">
+                </div>`  
     }
 }
 window.customElements.define('input-comp', InputComp);
