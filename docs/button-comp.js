b4063d77184b6e2d88ece0db9fa7b905751a67b6
Nalla Senthilnathan <nrsenthil@gmail.com>
2020-04-09 22:22:09 -0400

    button-comp.js revised

--- docs/button-comp.js
+++ docs/button-comp.js
@@ -3,20 +3,20 @@ class ButtonComp extends HTMLElement {
         return ['data-request'];
     }
     constructor() {
-      super();
+        super();
     }
 
     attributeChangedCallback(name, oldVal, newVal) {
-            if (newVal != null && newVal != undefined && newVal.length > 0 && name === 'data-request') {
-                const data = JSON.parse(newVal);
-                if(this.querySelector('input')==undefined){
-                    this.innerHTML = this.createInputButtonElement(data);
-                }   
-            }  
+        if (newVal != null && newVal != undefined && newVal.length > 0 && name === 'data-request') {
+            const data = JSON.parse(newVal);
+            if (this.getElementsByTagName(data.name)[0] == undefined) {
+                this.innerHTML = this.createInputButtonElement(data);
+            }
+        }
     }
 
     createInputButtonElement(data) {
-        return '<input type="button" name="' + data.name + '"  class="pure-button" value="' + data.value + '">';
-    }    
-  }
-  window.customElements.define('button-comp', ButtonComp);
+        return '<input type="button" id="button-comp-id" name="' + data.name + '"  class="pure-button" value="' + data.value + '">';
+    }
+}
+window.customElements.define('button-comp', ButtonComp);
